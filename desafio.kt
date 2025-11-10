2e2413da478b9d2be43ef92cc7c4e3094fc18506
Deimardfs <deimarserafim@gmail.com>
2025-11-10 14:47:15 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,53 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, *AVANCADO } // * para identificar e separara os alunos dentro dos respectivos Níveis
+
+*fun nivel = Nivel()
 
 class Usuario
+  
+    *fun usuario = Usuario()
+  )
+  var aluno: String
+  var idade: Int
+  var sexo: String
+  var nivel: String
+  var conteudoEducacional, String
+ )
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+/*data*/ class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+
+*fun conteudoEducacional = ConteudoEducacional()
+*val conteudoEducacional(
+    Java, 50
+    Kotlin, 45
+    SQL, 25
+    PHP, 20
+    Python, 40
+) // lista dos cursos disponíveis.
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+	
+    *when ConteudoEducacional(
+    Java -> )
+    
+    
     val inscritos = mutableListOf<Usuario>()
     
+    *inscritos.addAll(ListOf(Paulo, João, Maria)) // (https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/mutable-list-of.html)
+       
     fun matricular(usuario: Usuario) {
         TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-    }
+   
+    *fun usuario = Usuario()
+        *val usuario (
+        Paulo, 35, M, BASICO
+        João, 28, M, INTERMEDIARIO
+        Maria, 29, F, AVANCADO
+    )
 }
 
-fun main() {
+*fun main() {
     val nivel = Nivel.BASICO
     val message = when (nivel) {
         Nivel.BASICO -> "It´s Basico"
