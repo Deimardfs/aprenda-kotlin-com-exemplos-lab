a2a4a7fab648e4120716b1a2c8821d35c6487280
Deimardfs <deimarserafim@gmail.com>
2025-11-12 10:50:08 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -2,31 +2,56 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO } // * para identificar e separar os alunos dentro dos respectivos Níveis
 
-class Usuario ( var aluno: String, var idade: Int, var sexo: String )
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-var conteudoEducacional = listOf<ConteudoEducacional> ( 
-    "Java", 50
-    "Kotlin", 45
-    "SQL", 25
-    "PHP", 20
-    "Python", 40
+data class Usuario (
+    val aluno: String,
+    var idade: Int,
+    val sexo: String
     )
-// lista dos cursos (conteúdos) disponíveis.
 
-data class Formacao(val nome: String, val nivel: Nivel(), var conteudos: List<ConteudoEducacional>) {
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: String)
+
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 	
-  
-    
     val inscritos = mutableListOf<Usuario>()
         
     fun matricular(usuario: Usuario) {
         TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
       inscritos.addAll("João", 28, "M"; "Maria", 35, "F"; "Elder", 16, "M", "Lisa", 21, "F")) // (https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/mutable-list-of.html)
     }
-
     
+fun main(){
+
+val usuario1 = Usuario(
+    aluno = "João"
+    idade = 25
+    sexo = "M"
+)
+val usuario2 = Usuario(
+    aluno = "Maria"
+    idade = 25
+    sexo = "M"
+)
+val conteudoEducacional1 = ConteudoEducacional ( 
+    nome = "Java",
+    duracao = 50
+    nivel = BASICO
+   
+val conteudoEducacional2 = ConteudoEducacional ( 
+    nome = "Kotlin",
+    duracao = 45
+    nivel = BASICO   
+    
+ val conteudoEducacional3 = ConteudoEducacional ( 
+    nome = "SQL",
+    duracao = 25
+    nivel = BASICO
+ 
+ val conteudoEducacional4 = ConteudoEducacional ( 
+    nome = "PHP",
+    duracao = 20
+    nivel = BASICO
+    )
+print(usuario)
 }
 /*
 {
